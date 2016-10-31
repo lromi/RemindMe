629a0a01767f356570d601baf2c3b8cfdc3ad0bd
lromi <lromi@mail.ru>
2016-11-01 00:36:04 +0200

    Create MainActivity class and layout

---
+++ app/src/main/java/com/lromi/remindme/MainActivity.java
@@ -0,0 +1,17 @@
+package com.lromi.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by lromi on 31.10.2016.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
