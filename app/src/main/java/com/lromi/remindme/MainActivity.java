ad1e16e5244d6103f59c197785aa76bd3bf95581
lromi <lromi@mail.ru>
2016-11-02 15:36:36 +0200

    Add files to idea

--- app/src/main/java/com/lromi/remindme/MainActivity.java
+++ app/src/main/java/com/lromi/remindme/MainActivity.java
@@ -2,6 +2,8 @@ package com.lromi.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 /**
  * Created by lromi on 31.10.2016.
@@ -9,9 +11,29 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
     }
+    private void initToolbar(){
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
+
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
+
+    }
+
+
 }
